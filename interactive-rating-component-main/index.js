5497e78ab4b2fa591f483b6c34d4b86775eaeee6
A-D-11 <104553483+A-D-11@users.noreply.github.com>
2023-05-25 20:34:11 +0530

    Add files via upload

---
+++ interactive-rating-component-main/index.js
@@ -0,0 +1,26 @@
+const mainContainer = document.querySelector(".main-container")
+const thanksContainer = document.querySelector(".thank-you")
+const submitbutton = document.getElementById("submit")
+const rateAgain = document.getElementById("rate-again")
+const rating = document.getElementById("rating")
+const rates = document.querySelectorAll(".btn")
+
+
+submitbutton.addEventListener("click", () => {
+    thanksContainer.classList.remove("hidden")
+    mainContainer.style.display="none"
+})
+
+
+rateAgain.addEventListener("click", () => {
+   thanksContainer.classList.add("hidden")
+    mainContainer.style.display="block"
+})
+
+
+rates.forEach((rate) => {
+    rate.addEventListener("click", () => {
+        rating.innerHTML = rate.innerHTML
+    })
+    
+});
